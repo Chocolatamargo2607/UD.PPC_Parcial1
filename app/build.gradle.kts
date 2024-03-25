8bfc379cd1fb0c0d66b05ee586c43bc9b0330a62
Daniel Florez <daflorezm@udistrital.edu.co>
2024-03-24 23:14:34 -0500

    Add Room dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("android") version "1.5.31"
+    kotlin("kapt") version "1.5.31"
+
 }
 
 android {
@@ -48,6 +51,33 @@ android {
 
 dependencies {
 
+
+    implementation("androidx.room:room-ktx:2.6.1")
+
+
+    implementation ("androidx.room:room-runtime:2.6.1")
+    annotationProcessor ("androidx.room:room-compiler:2.6.1")
+
+    // To use Kotlin annotation processing tool (kapt)
+    kapt ("androidx.room:room-compiler:2.6.1")
+    // To use Kotlin Symbol Processing (KSP)
+    ksp ("androidx.room:room-compiler:2.6.1")
+
+    // optional - RxJava2 support for Room
+    implementation ("androidx.room:room-rxjava2:2.6.1")
+
+    // optional - RxJava3 support for Room
+    implementation ("androidx.room:room-rxjava3:2.6.1")
+
+    // optional - Guava support for Room, including Optional and ListenableFuture
+    implementation ("androidx.room:room-guava:2.6.1")
+
+    // optional - Test helpers
+    testImplementation ("androidx.room:room-testing:2.6.1")
+
+    // optional - Paging 3 Integration
+    implementation("androidx.room:room-paging:2.6.1")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
@@ -64,4 +94,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
 }
---
+++ app/src/main/java/com/example/udppc_parcial1/model/Song.java
@@ -0,0 +1,10 @@
+package com.example.udppc_parcial1.model;
+
+//@Entity(tableName = "song_table")
+
+public class Song {
+    int id;
+    String songTitle;
+    String songLyrics;
+
+}
