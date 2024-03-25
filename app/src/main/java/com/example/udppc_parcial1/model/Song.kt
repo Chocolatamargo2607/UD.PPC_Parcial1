7c2153e350179543a356ef689675c8c62ce80073
Daniel Florez <daflorezm@udistrital.edu.co>
2024-03-25 14:07:48 -0500

    Revert?

--- app/src/main/java/com/example/udppc_parcial1/model/Song.kt
+++ app/src/main/java/com/example/udppc_parcial1/model/Song.kt
@@ -1,11 +1,3 @@
 package com.example.udppc_parcial1.model
 
-import android.adservices.adid.AdId
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-@Entity
-data class Song(
-    @PrimaryKey var songId: Int,
-    @ColumnInfo var songLyrics: String
-)
+data class Song()
