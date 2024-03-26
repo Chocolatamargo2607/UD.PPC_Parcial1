8c67fa0dc7f6db4629405c8093b16c0893e7c312
Daniel Florez <daflorezm@udistrital.edu.co>
2024-03-25 21:44:00 -0500

    Add DAO and DB Entities, data management

--- app/src/main/java/com/example/udppc_parcial1/model/Song.kt
+++
@@ -1,3 +0,0 @@
-package com.example.udppc_parcial1.model
-
-data class Song()
--- app/src/main/java/com/example/udppc_parcial1/view/Save_letter_screen.kt
+++ app/src/main/java/com/example/udppc_parcial1/view/Save_letter_screen.kt
@@ -1,9 +1,31 @@
 package com.example.udppc_parcial1.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 
 @Composable
 fun Save_letter_screen(navController: NavController){
 
+}
+
+@Composable
+fun MessageCard(name:String){
+    Text(text = "Guardar letra")
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = "Guardar Cancion")
+        
+    }
+    Column {
+
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard(){
+    MessageCard("Android")
 }
