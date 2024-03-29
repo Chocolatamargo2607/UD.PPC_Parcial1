1030024797164fa826a6285ad39c41b42349fa97
~Chocolxt~ <dhsepulvedas@udistrital.edu.co>
2024-03-29 18:05:22 -0500

    Including drum sound

--- app/src/main/java/com/example/udppc_parcial1/view/Tambor_screen.kt
+++ app/src/main/java/com/example/udppc_parcial1/view/Tambor_screen.kt
@@ -1,6 +1,9 @@
 package com.example.udppc_parcial1.view
 
+import android.media.MediaPlayer
+import android.widget.ImageButton
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -30,15 +33,18 @@ import com.example.udppc_parcial1.view_model.navegation.App_screens
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Tambor_screen(navController: NavController){
-
+    val context = LocalContext.current
+    val mp: MediaPlayer = MediaPlayer.create(context,R.raw.song_tambor)
     Box(modifier = Modifier.fillMaxSize()){
         Image(
             painter = painterResource(id = R.drawable.tambor),
             contentScale = ContentScale.FillBounds ,
-            modifier = Modifier.align(Alignment.Center).size(260.dp),
-            contentDescription = "Fondo")
-
-
+            modifier = Modifier
+                .align(Alignment.Center)
+                .size(260.dp)
+                .clickable {mp.start() },
+            contentDescription = "Fondo",
+        )
     }
     Row(){
         TopAppBar(
