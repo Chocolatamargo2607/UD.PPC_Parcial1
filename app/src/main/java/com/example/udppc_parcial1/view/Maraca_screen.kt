08ad036aa2278bcefb399a15afb79f7097684bf3
~Chocolxt~ <dhsepulvedas@udistrital.edu.co>
2024-03-30 01:20:22 -0500

    Maraca sound

--- app/src/main/java/com/example/udppc_parcial1/view/Maraca_screen.kt
+++ app/src/main/java/com/example/udppc_parcial1/view/Maraca_screen.kt
@@ -1,5 +1,11 @@
 package com.example.udppc_parcial1.view
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.media.MediaPlayer
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -20,12 +26,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 import androidx.navigation.NavController
 import com.example.udppc_parcial1.R
 import com.example.udppc_parcial1.view_model.navegation.App_screens
+import kotlin.math.absoluteValue
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -36,9 +47,11 @@ fun Maraca_screen(navController: NavController){
         Image(
             painter = painterResource(id = R.drawable.maracas),
             contentScale = ContentScale.FillBounds ,
-            modifier = Modifier.align(Alignment.Center).size(260.dp),
+            modifier = Modifier
+                .align(Alignment.Center)
+                .size(260.dp),
             contentDescription = "Fondo")
-
+        ShakeSound()
 
     }
     Row(){
@@ -67,3 +80,54 @@ fun Maraca_screen(navController: NavController){
 fun Maraca_screen_preview() {
     Maraca_screen(NavController(LocalContext.current))
 }
+
+@Composable
+fun ShakeSound() {
+    val context = LocalContext.current
+    val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    val accelerometerSensor: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+    val mediaPlayer = MediaPlayer.create(context, R.raw.maraca_sound)
+
+    val sensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent?) {
+
+            // Verificar si el evento es un cambio de sensor de acelerómetro
+            if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+                val x = event.values[0]
+                val y = event.values[1]
+                val z = event.values[2]
+
+                val acceleration = Math.sqrt((x * x + y * y + z * z).toDouble())
+                if (acceleration > SHAKE_THRESHOLD) {
+                    mediaPlayer.start()
+                }
+                val speed = acceleration.toInt().absoluteValue
+
+                // Calcular el volumen en función de la velocidad
+                val volume = (speed / SHAKE_MAX_SPEED.toFloat()).coerceIn(0f, 1f)
+
+                // Ajustar el volumen del MediaPlayer
+                mediaPlayer.setVolume(volume, volume)
+            }
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        }
+    }
+
+    LocalLifecycleOwner.current.lifecycle.addObserver(object : LifecycleEventObserver {
+        override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+            if (event == Lifecycle.Event.ON_RESUME) {
+                sensorManager.registerListener(sensorEventListener, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL)
+            } else if (event == Lifecycle.Event.ON_PAUSE) {
+                sensorManager.unregisterListener(sensorEventListener)
+            }
+        }
+    })
+}
+
+
+private const val SHAKE_THRESHOLD = 20.0f
+
+// Velocidad máxima de agitación del teléfono
+private const val SHAKE_MAX_SPEED = 50
